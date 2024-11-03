e5b4b72d15d1fa8fe35f103fbb833a3d4d379199
István Antal <istvan@antal.xyz>
2024-11-03 08:14:20 +0000

    Add custom port

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
-import react from '@vitejs/plugin-react-swc'
+import deno from "@deno/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [deno(), react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".");
+  return ({
+    plugins: [deno(), react()],
+    server: {
+      port: parseInt(env.VITE__PORT),
+    },
+  });
+});
