dc44703ca99f76f5ff7bc13595255ae1f2ff8aa4
István Antal <istvan@antal.xyz>
2025-01-06 13:44:23 +0000

    Add source icons

--- src/home/components/SourceIcon.tsx
+++ src/home/components/SourceIcon.tsx
@@ -6,6 +6,10 @@ export const SourceIcon = ({ source }: { source: string }) => {
             return <img src="/sources/upwork.svg" alt="UpWork" width={24} height={24} />;
         case 'total_jobs':
             return <img src="/sources/total_jobs.svg" alt="UpWork" width={24} height={24} />;
+        case 'cv_library':
+            return <img src="/sources/cv_library.png" alt="UpWork" width={24} height={24} />;
+        case 'reed':
+            return <img src="/sources/reed.png" alt="UpWork" width={24} height={24} />;
         default:
             return (
                 <div className="size-[24px] flex items-center overflow-hidden bg-sky-400 rounded-md border-2 border-blue-700 b">
