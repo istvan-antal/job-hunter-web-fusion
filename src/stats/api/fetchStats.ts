e94a4ba782a37763167f42952a9d8fa8410d0026
István Antal <istvan@antal.xyz>
2025-08-25 12:52:39 +0100

    Show applied rates
    Signed-off-by: István Antal <istvan@antal.xyz>

--- src/stats/api/fetchStats.ts
+++ src/stats/api/fetchStats.ts
@@ -2,7 +2,10 @@ import { Job } from '../../../../job-hunter/entities/Job';
 import type { Context } from '../../core/context';
 import { dataSource } from '../../core/db';
 
-async function fetchJobs(granularity: 'daily' | 'weekly' | 'monthly' | 'yearly', _: Context): Promise<{
+async function fetchJobs(
+    granularity: 'daily' | 'weekly' | 'monthly' | 'yearly',
+    _: Context,
+): Promise<{
     rates: { id: string; data: { value: number; date: string }[] }[];
     counts: { id: string; data: { value: number; date: string }[] }[];
 }> {
@@ -10,11 +13,16 @@ async function fetchJobs(granularity: 'daily' | 'weekly' | 'monthly' | 'yearly',
 
     const getDateTrunc = (granularity: string) => {
         switch (granularity) {
-            case 'daily': return 'DATE(j.created)';
-            case 'weekly': return 'DATE_TRUNC(\'week\', j.created)';
-            case 'monthly': return 'DATE_TRUNC(\'month\', j.created)';
-            case 'yearly': return 'DATE_TRUNC(\'year\', j.created)';
-            default: return 'DATE(j.created)';
+            case 'daily':
+                return 'DATE(j.created)';
+            case 'weekly':
+                return "DATE_TRUNC('week', j.created)";
+            case 'monthly':
+                return "DATE_TRUNC('month', j.created)";
+            case 'yearly':
+                return "DATE_TRUNC('year', j.created)";
+            default:
+                return 'DATE(j.created)';
         }
     };
 
@@ -54,6 +62,26 @@ async function fetchJobs(granularity: 'daily' | 'weekly' | 'monthly' | 'yearly',
                             "payRate" ->> 'type' = 'daily' AND 
                             "isInsideIr35" IS FALSE AND 
                             "suggestApply" IS TRUE`,
+                        )
+                        .groupBy(dateTrunc)
+                        .orderBy(dateTrunc, 'DESC')
+                        .getRawMany()
+                ).map((item) => ({
+                    ...item,
+                    value: +item.value,
+                })),
+            },
+            {
+                id: 'Applied',
+                data: (
+                    await repository
+                        .createQueryBuilder('j')
+                        .select(`ROUND(AVG(("payRate" ->> 'max')::FLOAT)) AS "value", ${dateTrunc} as "date"`)
+                        .where(
+                            `"payRate" != '{"type": "unknown"}' AND 
+                            "payRate" ->> 'currency' = 'GBP' AND 
+                            "payRate" ->> 'type' = 'daily' AND 
+                            "applied" IS TRUE`,
                         )
                         .groupBy(dateTrunc)
                         .orderBy(dateTrunc, 'DESC')
